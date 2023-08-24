8965d20bfa6954d849c9e879a659bb036b385ea7
José Carlos <69639102+josecarlossoares@users.noreply.github.com>
2023-08-24 12:01:18 +0000

    Resolução do exercicio sobre objetos.

---
+++ script.js
@@ -0,0 +1,47 @@
+//primeiro
+// let vazio = {}
+
+//segundo
+// let pessoa = {}
+// pessoa.nome = 'joão'
+// console.log(pessoa)
+
+//terceiro
+// let usuario = {nome:'pedro', idade:21}
+// console.log(usuario.idade)
+
+//quarto
+// let produto = {preco:50}
+// produto.preco = 75
+// console.log(produto.preco)
+
+//quinto
+// let contato = {nome:'', email:'hdasgdjh@gmail.com'}
+// delete contato.email
+// console.log(contato)
+
+//sexto
+// let cliente = {nome:'', idade:21}
+// cliente.endereco = 'Rua A, 123'
+// console.log(cliente)
+
+//setimo
+// let empresa = {departamentos:['primeiro', 'segundo', 'terceiro']}
+// console.log(empresa.departamentos[0])
+
+//oitavo
+// let alunos = {notas:[85, 90, 78]}
+// alunos.notas[1] = 95
+// console.log(alunos.notas)
+
+//nono
+// let dados = {valores:[10, 20, 30]}
+// dados.valores.splice(1,1)
+// console.log(dados.valores)
+
+//decimo
+// let biblioteca = {
+//     titulo: 'livro',
+//     autor: 'Maria'
+// }
+// console.log(biblioteca)
